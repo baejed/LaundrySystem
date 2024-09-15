58cb403300648cb60cd2b2a2f5aea099ee73af2e
Joseph Tristan Subong <tristansubong@gmail.com>
2024-09-15 20:59:39 +0800

    did somethings

--- LaundrySystem/Laundries.Designer.cs
+++ LaundrySystem/Laundries.Designer.cs
@@ -36,8 +36,16 @@
             this.Amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Staff = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LblLaundryContents = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.GarmentType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LaundryContentAmount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnLaundryFinished = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -45,6 +53,10 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btnLaundryFinished);
+            this.panel1.Controls.Add(this.dataGridView2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.LblLaundryContents);
             this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
@@ -62,10 +74,10 @@
             this.Amount,
             this.Status,
             this.Staff});
-            this.dataGridView1.Location = new System.Drawing.Point(508, 3);
+            this.dataGridView1.Location = new System.Drawing.Point(508, 50);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.Size = new System.Drawing.Size(524, 564);
+            this.dataGridView1.Size = new System.Drawing.Size(524, 517);
             this.dataGridView1.TabIndex = 0;
             // 
             // LaundryID
@@ -98,6 +110,64 @@
             this.Staff.HeaderText = "Staff";
             this.Staff.Name = "Staff";
             // 
+            // LblLaundryContents
+            // 
+            this.LblLaundryContents.AutoSize = true;
+            this.LblLaundryContents.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblLaundryContents.Location = new System.Drawing.Point(3, 3);
+            this.LblLaundryContents.Name = "LblLaundryContents";
+            this.LblLaundryContents.Size = new System.Drawing.Size(182, 25);
+            this.LblLaundryContents.TabIndex = 1;
+            this.LblLaundryContents.Text = "Laundry Contents";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.GarmentType,
+            this.Quantity,
+            this.LaundryContentAmount});
+            this.dataGridView2.Location = new System.Drawing.Point(8, 50);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(494, 283);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // GarmentType
+            // 
+            this.GarmentType.HeaderText = "Garment Type";
+            this.GarmentType.Name = "GarmentType";
+            // 
+            // Quantity
+            // 
+            this.Quantity.HeaderText = "Quantity";
+            this.Quantity.Name = "Quantity";
+            // 
+            // LaundryContentAmount
+            // 
+            this.LaundryContentAmount.HeaderText = "Amount";
+            this.LaundryContentAmount.Name = "LaundryContentAmount";
+            // 
+            // btnLaundryFinished
+            // 
+            this.btnLaundryFinished.Location = new System.Drawing.Point(8, 421);
+            this.btnLaundryFinished.Name = "btnLaundryFinished";
+            this.btnLaundryFinished.Size = new System.Drawing.Size(75, 23);
+            this.btnLaundryFinished.TabIndex = 3;
+            this.btnLaundryFinished.Text = "Finished";
+            this.btnLaundryFinished.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(503, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ongoing Laundries";
+            // 
             // Laundries
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -107,7 +177,9 @@
             this.Name = "Laundries";
             this.Text = "Laundries";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -122,5 +194,12 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Amount;
         private System.Windows.Forms.DataGridViewTextBoxColumn Status;
         private System.Windows.Forms.DataGridViewTextBoxColumn Staff;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label LblLaundryContents;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GarmentType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LaundryContentAmount;
+        private System.Windows.Forms.Button btnLaundryFinished;
+        private System.Windows.Forms.Label label1;
     }
 }
